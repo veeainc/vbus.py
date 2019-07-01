968bb00680948f03db5dcaa682330a456090176a
mmardinian <mmardinian@veea.com>
2019-07-01 10:34:59 +0200

    add setup.py to be able to do a "pip install"

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='vbus',
+    version='0.1.0',
+    description='vBus client',
+    long_description='vBus client',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+        ],
+    url='https://vbus@bitbucket.org/vbus/vbus.py.git',
+    author='Michael Mardinian',
+    author_email='mmardinian@veea.com',
+    license='To Be Define',
+    packages=['vbus'],
+    zip_safe=True,
+    extras_require=EXTRAS
+)
