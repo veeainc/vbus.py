d9ae906f92d1895fe4e7a88edc5755fe9df9274a
mmardinian <mmardinian@veea.com>
2019-07-02 22:14:51 +0200

    you got to be kiding me

--- vbus/vbus.py
+++ vbus/vbus.py
@@ -106,7 +106,7 @@ class Client(NATS):
         if rootfolder == "":
             rootfolder = os.environ['HOME']
             rootfolder = rootfolder + "/vbus/"
-        elif rootfolder.endswith("/") == FALSE
+        elif rootfolder.endswith("/") == False:
             rootfolder = rootfolder + "/"
         if os.access(rootfolder, os.F_OK) == False:
             os.mkdir(rootfolder)
