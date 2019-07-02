1dcf5ff26be765727f4f8159f1cc5506416fe810
mmardinian <mmardinian@veea.com>
2019-07-02 07:09:25 +0200

    still confused by int and string in python... and NoneType :/

--- vbus/vbus.py
+++ vbus/vbus.py
@@ -66,7 +66,7 @@ def zeroconf_search():
             # next step compare host_name to choose the same one than the service if available
                 print("vbus found !!")
                 #if z_vbus_url == None:
-                z_vbus_url = "nats://" + socket.inet_ntoa(cast(bytes, info.address))+ ":" + info.port
+                z_vbus_url = "nats://" + socket.inet_ntoa(cast(bytes, info.address))+ ":" + str(info.port)
                 print("zeroconf reconstruct: " + z_vbus_url)
 
     zeroconf = Zeroconf()
