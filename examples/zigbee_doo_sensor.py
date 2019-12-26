1453d2eba5830689b4d75da3b4e4749f16e4bc51
Eliott Dumeix <eliott.dumeix@veeasystems.com>
2019-12-26 11:04:50 +0100

    fix door sensor example.

--- examples/zigbee_doo_sensor.py
+++ examples/zigbee_doo_sensor.py
@@ -1,3 +1,12 @@
+"""
+    Example that demonstrate how to pair and enroll a Heiman door sensor (IAS Zone cluster) trough
+    the Zigpy Vbus module.
+
+    Steps are:
+        - set cie_addr attribute with the controlle ieee address
+        - wait enroll client commands
+        - respond to enroll with an enroll_response server command
+"""
 import sys
 import asyncio
 import logging
@@ -10,9 +19,8 @@ CONTROLLER_IEEE = [0x00, 0x0d, 0x6f, 0x00, 0x0b, 0x44, 0x8e, 0xaf]
 HOST = 'boolangery-ThinkPad-P1-Gen-2'
 
 
-async def on_client_command(data):
-    print('received enrollment command')
-    return [0, 0]
+async def on_state_changed(data):
+    print("state changed: ", data)
 
 
 async def main():
@@ -28,8 +36,14 @@ async def main():
         sys.exit("sensor not found")
 
     cmd = await sensor_node.get_attribute('endpoints', '1', 'in_clusters', '1280', 'client_commands', '1')
-    if not cmd:
+    srv = await sensor_node.get_attribute('endpoints', '1', 'in_clusters', '1280', 'server_commands', '0')
+    if not cmd or not srv:
         sys.exit("cannot find client command 1")
+
+    async def on_client_command(data):
+        print('received enrollment command')
+        enroll_resp = [0, 0]
+        await srv.set(enroll_resp)
 
     await cmd.subscribe_set(on_set=on_client_command)
 
@@ -37,7 +51,14 @@ async def main():
     if not cie_attr:
         sys.exit("cannot find cie attr")
 
+    print("sending cie address")
     await cie_attr.set(CONTROLLER_IEEE)
+
+    state_attr = await sensor_node.get_attribute('endpoints', '1' ,'in_clusters', '1280', 'client_commands', '0')
+    if not state_attr:
+        sys.exit("cannot find state attr")
+
+    await state_attr.subscribe_set(on_set=on_state_changed)
 
     stopped = asyncio.Event()
     await stopped.wait()
