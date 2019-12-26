74f7a10db730813f527f60e3453a4001efd03615
Eliott Dumeix <eliott.dumeix@veeasystems.com>
2019-12-26 09:52:48 +0100

    update examples.

--- examples/zigbee_doo_sensor.py
+++ examples/zigbee_doo_sensor.py
@@ -1,12 +1,18 @@
+import sys
 import asyncio
-from vbus import Client
 import logging
+from vbus import Client
 
 logging.basicConfig(level=logging.DEBUG)
 
 SENSOR_IEEE = '00:0d:6f:00:11:08:71:f7'
 CONTROLLER_IEEE = [0x00, 0x0d, 0x6f, 0x00, 0x0b, 0x44, 0x8e, 0xaf]
 HOST = 'boolangery-ThinkPad-P1-Gen-2'
+
+
+async def on_client_command(data):
+    print('received enrollment command')
+    return [0, 0]
 
 
 async def main():
@@ -19,38 +25,19 @@ async def main():
     sensor_node = await nodes.get_node(HOST, SENSOR_IEEE)
 
     if not sensor_node:
-        print("sensor not found")
-        return
-
-    # system.zigbee.boolangery-ThinkPad-P1-Gen-2.00:0d:6f:00:11:08:71:f7.endpoints.1.in_clusters.1280.client_commands.1.set
-    async def on_client_command(data):
-        pass
-        print(data)
-        return [0, 0]
-        # command 1 is enroll request
-        #if int(cmd_id) == 1:
-        #    data = [0, 0]
-        #    await door_sensor.publish('endpoints', 1, 'in_clusters', 1280, 'server_commands', 0, data=data)
+        sys.exit("sensor not found")
 
     cmd = await sensor_node.get_attribute('endpoints', '1', 'in_clusters', '1280', 'client_commands', '1')
     if not cmd:
-        print("cannot find client command 1")
-        return
+        sys.exit("cannot find client command 1")
 
     await cmd.subscribe_set(on_set=on_client_command)
 
     cie_attr = await sensor_node.get_attribute('endpoints', '1', 'in_clusters', '1280', 'attributes', '16')
     if not cie_attr:
-        print("cannot find cie attr")
-        return
+        sys.exit("cannot find cie attr")
 
     await cie_attr.set(CONTROLLER_IEEE)
-
-    #await door_sensor.subscribe('endpoints', 1, 'in_clusters', 1280, 'client_commands', '*',
-    #                            cb=on_client_command)
-    #await door_sensor.publish('endpoints', 1, 'in_clusters', 1280, 'attributes', 16, data=CONTROLLER_IEEE)
-
-
 
     stopped = asyncio.Event()
     await stopped.wait()
