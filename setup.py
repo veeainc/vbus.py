a1daa1f7a42c7669dc5f2b05a46cf84d1a66a03b
Eliott Dumeix <eliott.dumeix@veeasystems.com>
2020-01-17 11:18:29 +0100

    update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vbus',
-    version='0.1.0',
+    version='1.0.0',
     description='vBus client',
     long_description='vBus client',
     classifiers=[
