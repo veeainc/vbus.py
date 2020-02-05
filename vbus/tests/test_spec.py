b82ad0cfa9a2e044af0362665c897a3a14facf0d
Eliott Dumeix <eliott.dumeix@veeasystems.com>
2020-02-05 19:58:26 +0100

    add attribute tests.

--- vbus/tests/test_spec.py
+++ vbus/tests/test_spec.py
@@ -2,8 +2,7 @@ import sys
 import unittest
 import vbus
 import logging
-from vbus.tests.utils import async_test, setup_test
-import natsplayer
+from vbus.tests.utils import async_test, setup_test, retry
 
 
 class TestStringMethods(unittest.TestCase):
@@ -19,14 +18,48 @@ class TestStringMethods(unittest.TestCase):
         client = await self.new_client()
 
         resp = await client.ask_permission("should.be.true")
-        self.assertEquals(resp, True)
+        self.assertEqual(resp, True)
 
         resp = await client.ask_permission("should.be.false")
-        self.assertEquals(resp, False)
+        self.assertEqual(resp, False)
+
+        self.assertTrue(player.is_success())
+
+    @async_test
+    async def test_add_attribute(self):
+        player = setup_test("./scenarios/add_attribute.json")
+        client = await self.new_client()
+
+        attr = await client.add_attribute("name", "HEIMAN")
+        self.assertIsNotNone(attr)
+
+        self.assertTrue(player.is_success())
+
+    @async_test
+    async def test_set_attribute(self):
+        player = setup_test("./scenarios/set_attribute.json")
+        client = await self.new_client()
+
+        attr = await client.add_attribute("name", "HEIMAN")
+        self.assertIsNotNone(attr)
+        await attr.set_value("hello world")
+
+        self.assertTrue(player.is_success())
+
+    @async_test
+    async def test_get_remote_attribute(self):
+        player = setup_test("./scenarios/remote_attribute_get.json")
+        client = await self.new_client()
+
+        remote_attr = await client.get_remote_attr("test", "remote", client.hostname, "name")
+        self.assertIsNotNone(remote_attr)
+        val = await remote_attr.get_value()
+        self.assertEqual("HEIMAN", val)
 
         self.assertTrue(player.is_success())
 
     @staticmethod
+    @retry(Exception, tries=4)
     async def new_client():
         client = vbus.Client("test", "vbuspy")
         await client.connect()
