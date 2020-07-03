da6f79ea9dae1f87e6dc3085bcea1cf5392da9c7
Eliott Dumeix <eliott.dumeix@veeasystems.com>
2020-07-03 09:26:42 +0200

    bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vbus',
-    version='1.1.0',
+    version='1.2.0',
     description='vBus client',
     long_description='vBus client',
     classifiers=[
