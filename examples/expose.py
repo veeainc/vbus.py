aeca494187e3650c79b9dc081748b95c16f2c26b
Eliott Dumeix <eliott.dumeix@veeasystems.com>
2020-08-11 17:18:43 +0200

    examples: improve expose.

--- examples/expose.py
+++ examples/expose.py
@@ -2,19 +2,29 @@
     This example demonstrate how to expose an uri.
 """
 import asyncio
-
+import http.server
+import socketserver
+from http import HTTPStatus
 from vbus import Client
 import logging
 
-
 logging.basicConfig(level=logging.DEBUG)
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        self.wfile.write(b'Hello world')
 
 
 async def main():
     client = Client("system", "client_python")
     await client.connect()
 
-    await client.expose("frontend", 'http', '80', 'google')
+    await client.expose("frontend", 'http', 21800)
+
+    httpd = socketserver.TCPServer(('', 21800), Handler)
+    httpd.serve_forever()
 
     stopped = asyncio.Event()
     await stopped.wait()
